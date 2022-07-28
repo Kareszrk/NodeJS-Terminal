43064dd87f8b0d68fea49798dd94ae5f4f43cd77
root <root@vmi946211.contaboserver.net>
2022-07-28 13:19:01 +0200

    Complete Terminal Example to NodeJS commit

--- src/app.ts
+++ src/app.ts
@@ -14,24 +14,49 @@ http.listen(3000, () => {
 io.on("connection", (socket:any) => {
     console.log('a user connected');
     let out_to_client = processor.currentContent;
-    io.emit('consoleContent', out_to_client.slice(-80));
+    io.emit('consoleContent', out_to_client.slice(-50));
+    // Receiving messages from clients via socket:
+    socket.on('sendConsoleCommand', (args) => {
+        processor.sendCommand(args); // Here we pass through the message to the processor
+    });
+
+    socket.on('startServer', () => {
+        // We pass the request to processor
+        processor.startServer().then(() => {
+            // Processor resolved the request, so it has been started as requested
+            socket.emit("state", "started");
+        }).catch(() => {
+            // Processor rejected the request, this means the service already running
+            socket.emit("state", "already");
+        });
+    });
+
+    socket.on('killServer', () => {
+        // We pass the request to processor
+        processor.killServer().then(() => {
+            // Processor resolved the request, so it has been killed as requested
+            socket.emit("state", "killed");
+        }).catch(() => {
+            // Processor rejected the request, this means the service already killed
+            socket.emit("state", "not-running");
+        });
+    });
+
+    // Event from client via socket
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
-});
-
-io.on('sendConsoleCommand', (args: string) => {
-    processor.sendCommand(args);
 });
 
 app.get('/', (req:any, res:any) => {
     res.sendFile(path.resolve('public/console.html'));
 });
 
+// This exported function stands for transporting the required message (which is an array of strings) to the frontend via socket message.
 export const ConsoleSender = (message:string[]) => {
-    io.emit('consoleContent', message.slice(-80));
+    // We send the information on the frontend socket listener.
+    io.emit('consoleContent', message.slice(-50));
 };
 
-processor.startServer();
-
-console.log("Works");
+// A simple feedback for the service issuer to know everything started successfully.
+console.log("Microservice is running");
--- src/processor.ts
+++ src/processor.ts
@@ -1,24 +1,58 @@
 import { spawn } from 'node:child_process';
 import { ConsoleSender } from './app';
+import path from 'path';
+
+const __dirname = path.resolve(path.dirname(''));
 
 export class processor {
     // Running child process in NodeJS source documentation: https://nodejs.org/api/child_process.html
-    software: any;
-    currentContent: string[];
+    // We declare the types of varriables.
+    software: any; // It literally can be anything it want.
+    currentContent: string[]; // It's an array of strings
     constructor () {
-        this.software;
+        // We declare the publicly accessible varriables (avaliable for other files which calls this);
+        this.software = false;
         this.currentContent = [];
     }
+    // We make a function inside this class (publicly avaliable), when it gets called it starts the software in node's child process.
     startServer = () => {
-        console.log("Starting server...");
-        this.software = spawn('java', ['-jar', '/var/minecraft/start.jar'], {cwd: "/var/minecraft"});
-        this.software.stdout.on('data', (data) => {
-            this.currentContent.push(`${data}`);
-            ConsoleSender(this.currentContent);
+        return new Promise<void>((resolve, reject) => {
+            // We check whether or not the server is running already
+            if(this.software) return reject(); // It does so we to avoid problems we reject the request. (And we return it to close the request)
+            else {
+                this.software = spawn('java', ['-jar', `${__dirname}/server/start.jar`], {cwd: `${__dirname}/server`});
+                // We start a minecraft server by default within the /var/minecraft folder, defined in the options section in: "cwd"
+                // As soon as we issued the command we listen for all the updates in the console
+                resolve();
+                // We can kill this process later with this PID: console.log(this.software.pid);
+                this.software.stdout.on('data', (data) => {
+                    // When we detect changes in the console we save it into a public varriable
+                    this.currentContent.push(`${data}`);
+                    // Then we pass it to the client via socket
+                    ConsoleSender(this.currentContent);
+                });
+                // We put here a listener on software ends, when the programme finished we save it to this.software to ensure the server isn't running anymore.
+                this.software.on('close', (code) => {
+                    this.software = false; // We set this to false, so we can run the check at the beginning
+                });
+            }
+        });
+    }
+    killServer = () => {
+        return new Promise<void>((resolve, reject) => {
+            // We check whether or not the server is running or killed already
+            if(!this.software) return reject(); // This server is not running, so we reject the request.
+            else {
+                // We send a SIGNINT kill sign to the server.
+                this.software.kill("SIGINT");
+                this.software = false; // Here we set the software public varriable to false to run all the checks later correctly.
+                return resolve(); // We killed the software so we resolved this case.
+            }
         });
     }
     sendCommand = (command: string) => {
-        console.log(command);
-        this.software.stdin.write(`${command}\n`);
+        // We recieved a command to send it to the child process, so we send it to
+        if(this.software) this.software.stdin.write(`${command}\n`);
+        // If the software isn't running we just ignore this request.
     }
 }
